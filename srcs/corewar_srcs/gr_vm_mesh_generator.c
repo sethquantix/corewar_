2000efdbeb3236da0443a70858ffe2c14c37bc14
Rodolphe CARGOU <rcargou@e1r12p11.42.fr>
2017-04-09 20:54:14 +0200

    Going sleep

--- srcs/corewar_srcs/gr_vm_mesh_generator.c
+++ srcs/corewar_srcs/gr_vm_mesh_generator.c
@@ -49,12 +49,33 @@ static const float g_cube[] = {
 
 static void		push_plane(GLfloat *data)
 {
-	;
+	int i;
+
+	i = 0;
+	while (i < sizeof(g_cube) / (sizeof(float) * 6))
+	{
+		data = g_cube[i % (sizeof(g_cube) / sizeof(GLfloat))];
+		i++;
+		data++;
+	}
 }
 
 static GLfloat	*push_cube(GLfloat *data, float offx, float offy, float sz)
 {
-	;
+	int i;
+
+	i = 0;
+	while (i < sizeof(g_cube) / sizeof(float))
+	{
+		if ((i % 8) <= 2)
+			data = (g_cube[i % (sizeof(g_cube) /
+				sizeof(GLfloat))]) + offx + offy * sz;
+		else
+			data = g_cube[i % (sizeof(g_cube) / sizeof(GLfloat))];
+		i++;
+		data++;
+	}
+	return (data);
 }
 
 static GLfloat	*create_vertex(uint n)
@@ -74,7 +95,8 @@ static GLfloat	*create_vertex(uint n)
 		j = 0;
 		while (j < n)
 		{
-			data = push_cube(data, i, j, 1.0f / n);
+			data = push_cube(data, (i * 1.3) - (1.3 * n / 2),
+				(j * 1.3) - (1.3 * n / 2), 1.0f / (n * n));
 			j++;
 		}
 		i++;
@@ -84,7 +106,7 @@ static GLfloat	*create_vertex(uint n)
 }
 
 /*
- ** This function create a mesh with a "numCube" cube
+ ** This function create a mesh with a "n * n" cubes
  ** and return a vao ID to this mesh.
 */
 
@@ -94,5 +116,26 @@ GLuint			generate_cube(uint n)
 	GLuint		vaoid;
 
 	vertex = create_vertex(n);
+	glGenVertexArrays(1, &(vaoid));
 	return (vaoid);
+}
+
+uint32_t renderDataSys::createVBO_VNT(float *vertices, uint32_t vertex_size, uint32_t vaoId)
+{
+    GLuint  vertexBufferID;
+    GLuint  normalBufferID;
+    GLuint  textCoordBufferID;
+
+    glBindVertexArray(vaoId);
+
+    glGenBuffers(1, &(vertexBufferID));
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER,  vertex_size * sizeof(float) * 8, &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float)*3));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float)*6));
+    return vertexBufferID;
 }
