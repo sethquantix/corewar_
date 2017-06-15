259308e8a66729442ebd98080ba076abefdb86d2
cchaumar <cchaumar@student.42.fr>
2017-06-15 11:30:57 +0200

    Everything is working ! (math said that uv max is 100 / (scale + 10), who would have known x) ) still needs to work on process inspection, color is coming up. KEEP IT UP

--- includes/gr_vm.h
+++ includes/gr_vm.h
@@ -6,7 +6,7 @@
 /*   By: cchaumar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/06 12:28:52 by cchaumar          #+#    #+#             */
-/*   Updated: 2017/06/03 20:44:31 by cchaumar         ###   ########.fr       */
+/*   Updated: 2017/06/15 04:03:56 by cchaumar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include "gr_types.h"
 # include "crwr_structs.h"
 
-# define WIN_WIDTH	1280
-# define WIN_HEIGHT	720
+# define WIN_WIDTH	2560
+# define WIN_HEIGHT	1440
 
 void	gr_vm_init(t_gr_vm *cxt, t_arena *arena);
 void	gr_vm_run(t_vm_loop loop, void *data, t_gr_vm *ctx);
